5c71c8ef614de9dd3811c9b2b5762e3f014e96ae
unknown <ricardofantunes@ua.pt>
2023-03-15 23:31:23 +0000

    DDL

--- 3 ano/BD/Aula4/Ex1/SQLQuery1.sql
+++ 3 ano/BD/Aula4/Ex1/SQLQuery1.sql
@@ -1,9 +1,60 @@
-
-CREATE SCHEMA Rent_A_Car;
+DROP DATABASE rentacar;
+CREATE DATABASE rentacar;
 GO
 
-create table Rent_A_Car.balcao(
-	Endereco	VARCHAR(50),
-	Nome		VARCHAR(50),
-	Numero			INT			NOT NULL		PRIMARY KEY
+use rentacar;
+
+create table balcao(
+	Endereço		VARCHAR(50),
+	Numero	
+	INT			PRIMARY KEY		NOT NULL,
+	Nome			VARCHAR(50)
+);
+
+create table cliente(
+	Nome			VARCHAR(50),
+	Endereço		VARCHAR(50),
+	Num_carta		INT			UNIQUE,
+	NIF				INT			PRIMARY KEY		NOT NULL		
+);
+
+create table tipo_veiculo(
+	codigo		INT		PRIMARY KEY		NOT NULL,
+	Designação	VARCHAR(20), 
+	A_C			BIT
+);
+
+create table veiculo(
+	ano					CHAR(4),
+	marca				VARCHAR(20),
+	matricula			CHAR(6)		PRIMARY KEY		NOT NULL,
+	codigo_tipo_veiculo INT		REFERENCES	tipo_veiculo(codigo)
+);
+
+create table aluguer(
+	data	DATE,
+	numero	INT		PRIMARY KEY		NOT NULL,
+	duracao	TIME,
+	NIF_cliente			INT			REFERENCES cliente(NIF),
+	numero_balcao		INT			REFERENCES balcao(Numero),
+	matricula_veiculo	CHAR(6)		REFERENCES veiculo(matricula)
+);
+
+create table ligeiro(
+	numero_lugares			INT,
+	portas					INT,
+	combustivel				VARCHAR(10),
+	codigo_tipo_veiculo		INT		PRIMARY KEY		NOT NULL	REFERENCES tipo_veiculo(codigo)
+);
+
+create table pesado(
+	peso			INT,
+	passageiros		INT,
+	codigo_tipo_veiculo		INT PRIMARY KEY			NOT NULL	REFERENCES tipo_veiculo(Codigo)
+);
+
+create table similaridade(
+	codigo_1	INT		REFERENCES tipo_veiculo(codigo)	NOT NULL,
+	codigo_2	INT		REFERENCES tipo_veiculo(codigo) NOT NULL,
+	PRIMARY KEY(codigo_1,codigo_2)
 );
